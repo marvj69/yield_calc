67379210dd76bc9ad72a015c5715e1764a5c25bd
Mark Heinonen <heinonenmh@gmail.com>
2025-03-20 13:17:46 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,43 @@
+// sw.js
+const CACHE_NAME = 'asphalt-yield-calculator-v1';
+const urlsToCache = [
+  '/',         // Cache the root URL (resolves to index.html)
+  '/index.html' // Explicitly cache index.html
+];
+
+// Install event: Cache the specified resources
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event: Serve cached content when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached response if available, otherwise fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Activate event: Clean up old caches
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
